a0d9a09a5526754ec70acbfc8afd88d61f6749eb
Kevin Grandon <kevingrandon@yahoo.com>
2014-09-10 21:50:24 -0700

    Update to use a standalone library for mozapps.

--- js/apps.js
+++ js/apps.js
@@ -1,119 +1,41 @@
 (function() {
 
-	// Hidden manifest roles that we do not show
-	var HIDDEN_ROLES = ['system', 'keyboard', 'homescreen'];
-
 	// Apps container
 	var parent = document.getElementById('apps');
-
-	// List of all application icons
-	var icons = [];
-
-	/**
-	 * Represents a single app icon on the homepage.
-	 */
-	function Icon(app, entryPoint) {
-		this.app = app;
-		this.entryPoint = entryPoint;
-	}
-
-	Icon.prototype = {
-
-		get name() {
-			return this.descriptor.name;
-		},
-
-		get icon() {
-			var icons = this.descriptor.icons;
-			if (!icons) {
-				return '';
-			}
-
-			for (var i in icons) {
-				return icons[i];
-			}
-		},
-
-		get descriptor() {
-			if (this.entryPoint) {
-				return this.app.manifest.entry_points[this.entryPoint];
-			}
-			return this.app.manifest;
-		},
-
-		/**
-		 * Renders the icon to the container.
-		 */
-		render: function() {
-			if (!this.icon) {
-				return;
-			}
-
-			var tile = document.createElement('div');
-			tile.className = 'tile';
-			tile.dataset.origin = this.app.origin;
-			if (this.entryPoint) {
-				tile.dataset.entryPoint = this.entryPoint;
-			}
-			tile.style.backgroundImage = 'url(' + this.app.origin + this.icon + ')';
-
-			parent.appendChild(tile);
-		},
-
-		/**
-		 * Launches the application for this icon.
-		 */
-		launch: function() {
-			if (this.entryPoint) {
-				this.app.launch(this.entryPoint);
-			} else {
-				this.app.launch();
-			}
-		}
-	};
-
-	/**
-	 * Creates icons for an app based on hidden roles and entry points.
-	 */
-	function makeIcons(app) {
-		if (HIDDEN_ROLES.indexOf(app.manifest.role) !== -1) {
-			return;
-		}
-
-		if (app.manifest.entry_points) {
-			for (var i in app.manifest.entry_points) {
-				icons.push(new Icon(app, i));
-			}
-		} else {
-			icons.push(new Icon(app));
-		}
-	}
 
 	/**
 	 * Returns an icon for an element.
 	 * The element should have an entry point and origin in it's dataset.
 	 */
 	function getIconByElement(element) {
-		var elEntryPoint = element.dataset.entryPoint;
-		var elOrigin = element.dataset.origin;
+		var identifier = element.dataset.identifier;
+		return FxosApps.get(identifier);
+	}
 
-		for (var i = 0, iLen = icons.length; i < iLen; i++) {
-			var icon = icons[i];
-			if (icon.entryPoint === elEntryPoint && icon.app.origin === elOrigin) {
-				return icon;
-			}
+	/**
+	 * Renders the icon to the container.
+	 */
+	function render(icon) {
+		if (!icon.icon) {
+			return;
 		}
+
+		var tile = document.createElement('div');
+		tile.className = 'tile';
+		tile.dataset.identifier = icon.identifier;
+		tile.style.backgroundImage = 'url(' + icon.icon + ')';
+
+		parent.appendChild(tile);
 	}
 
 	/**
 	 * Fetch all apps and render them.
 	 */
-	navigator.mozApps.mgmt.getAll().onsuccess = function(event) {
-		event.target.result.forEach(makeIcons);
-		icons.forEach(function(icon) {
-			icon.render();
-		});
-	};
+	console.log('Fetching!')
+	FxosApps.all().then(icons => {
+		console.log('fetched ', icons.length)
+		icons.forEach(render);
+	});
 
 	/**
 	 * Add an event listener to launch the app on click.
